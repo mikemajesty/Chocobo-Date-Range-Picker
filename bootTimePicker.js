ca27f2a7ae595e1f24516d51cadae8e60b94f19e
mikemajesty <mikemajesty2011@gmail.com>
2016-12-04 19:00:20 -0200

    Add trimestre button

--- bootTimePicker.js
+++ bootTimePicker.js
@@ -11,12 +11,29 @@
             link: function (scope, elem, attrs, ngModel) {
                 scope.dataIni = new Date();
                 scope.dataFin = new Date();
-                angular.element(elem).on('change', function (event) {
 
+                var between = [];
+                between.push(new Date().toLocaleDateString(attrs.locale));
+                ngModel.$setViewValue(between);
+
+                angular.element(elem).on('change', function (event) {
+                    between = [];
+                    SetRangeDate();
+                });
+
+                scope.selectTrimestre = function () {
+                  var today = new Date();
+                  today.setMonth(today.getMonth() - 3)
+                  scope.dataIni = today;
+                  scope.dataFin = new Date();
+                  between = [];
+                  SetRangeDate();
+                }
+
+                function SetRangeDate() {
                   var start = scope.dataIni;
                   var end = scope.dataFin;
                   var currentDate = new Date(start.getTime());
-                  var between = [];
 
                   while (currentDate <= end) {
                       between.push(new Date(currentDate).toLocaleDateString(attrs.locale));
@@ -24,12 +41,10 @@
                   }
                   ngModel.$setViewValue(between);
                   ngModel.$render();
-                });
+                }
             },
             controller: function ($scope) {
-                $scope.selectTrimestre = function () {
-                    console.log('scope', $scope);
-                }
+
             },
 						templateUrl: '/timePicker.html'
         };
