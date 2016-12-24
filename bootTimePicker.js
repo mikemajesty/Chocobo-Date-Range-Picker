0c9d522487c6e10c47e600aabb6e944ef5500919
mikemajesty <mikemajesty2011@gmail.com>
2016-12-24 19:21:24 -0200

    Add another template

--- bootTimePicker.js
+++ bootTimePicker.js
@@ -140,6 +140,46 @@
             ngModel.$render();
             console.log('quantity ', between.length);
           }
+          //---------------------------------------
+          var app = {
+            /* settings: {
+               container: $('.calendar'),
+               calendar: $('.front'),
+               days: $('.weeks span'),
+               form: $('.back'),
+               input: $('.back input'),
+               buttons: $('.back button')
+             },
+ 
+             init: function () {
+               instance = this;
+               settings = this.settings;
+               this.bindUIActions();
+             },
+ 
+             swap: function (currentSide, desiredSide) {
+               settings.container.toggleClass('flip');
+ 
+               currentSide.fadeOut(900);
+               currentSide.hide();
+ 
+               desiredSide.show();
+             },
+ 
+             bindUIActions: function () {
+               settings.days.on('click', function () {
+                 instance.swap(settings.calendar, settings.form);
+                 settings.input.focus();
+               });
+ 
+               settings.buttons.on('click', function () {
+                 instance.swap(settings.form, settings.calendar);
+               });
+             }*/
+          }
+
+          //app.init();
+
         },
         templateUrl: '/timePicker.html'
       };
