2afcb174bebf88c08dc50ac644a364994d2ddcd4
mike.lima <mike.lima@tegra.me>
2017-01-05 14:32:27 -0200

    add flavicon

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
 
 app.use('/public',  express.static(__dirname + '/demo'));
 app.use('/',  express.static(__dirname + '/'));
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 var PORT = process.env.PORT || 3000;
 
