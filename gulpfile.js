197221b54524169ba39970dc6c901973a2ff5411
mikemajesty <mikemajesty2011@gmail.com>
2017-01-14 18:18:02 -0200

    add minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
+var uglifycss = require('gulp-uglifycss');
 
 gulp.task('js', function () {
 	return gulp.src('chocobo-range-picker.js')
@@ -12,3 +13,15 @@ gulp.task('js', function () {
 		.pipe(concat('chocobo-range-picker.min.js'))
 		.pipe(gulp.dest('dist/js/'));
 });
+
+gulp.task('css', function () {
+  gulp.src('chocobo-range-picker.css')
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+		.pipe(concat('chocobo-range-picker.min.css'))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('default', ['css', 'js']);
